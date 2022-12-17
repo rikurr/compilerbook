20723274054053637bb49177af1bd281a15712bf
oya riku <56904584+rikurr@users.noreply.github.com>
2022-12-17 13:44:59 +0900

    Add:単行演算子の+と-の処理を追加

--- 9cc.c
+++ 9cc.c
@@ -172,6 +172,7 @@ Node *new_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 
@@ -189,19 +190,30 @@ Node *expr() {
   }
 };
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
+      node = new_binary(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
+      node = new_binary(ND_DIV, node, unary());
     else
       return node;
   }
 };
+
+// unary = ("+" | "-")? primary
+Node *unary() {
+  if(consume('+')) 
+    return primary();
+
+  if(consume('-'))
+    return new_binary(ND_SUB, new_num(0), unary());
+
+  return primary();
+}
 
 // primary = "(" expr ")" | num
 Node *primary() {
